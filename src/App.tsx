dfd896d3ecae3402ed07b252e9b2af4e18f580e1
Eduardo Novais <eduardodepaulanovais10@gmail.com>
2025-11-16 12:05:09 -0300

    feat: atualizar rotas do App com guards condicionais

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,18 @@ import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import Cadastro from "./pages/Cadastro";
 import Tour from "./pages/Tour";
-import Programa from "./pages/Programa";
-import Agenda from "./pages/Agenda";
-import Diario from "./pages/Diario";
-import Conteudos from "./pages/Conteudos";
-import WaitingRoom from "./pages/WaitingRoom";
-import Prontuario from "./pages/Prontuario";
+import Jornada from "./pages/jornada";
+import Agenda from "./pages/agenda";
+import Diario from "./pages/diario";
+import Conteudos from "./pages/conteudos";
+import CheckinPeriodico from "./pages/check-in-periodico";
+import CompletarPerfil from "./pages/CompletarPerfil";
+import Prontuario from "./pages/prontuario";
 import AdminPanel from "./pages/AdminPanel";
+import Perfil from "./pages/perfil";
+import Notificacoes from "./pages/notificacoes";
 import NotFound from "./pages/NotFound";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
 const queryClient = new QueryClient();
 
@@ -26,19 +30,83 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter
+          future={{
+            v7_startTransition: true,
+            v7_relativeSplatPath: true,
+          }}
+        >
           <Routes>
             <Route path="/" element={<Landing />} />
             <Route path="/login" element={<Login />} />
             <Route path="/cadastro" element={<Cadastro />} />
             <Route path="/tour" element={<Tour />} />
-            <Route path="/waiting-room" element={<WaitingRoom />} />
-            <Route path="/programa" element={<Programa />} />
-            <Route path="/agenda" element={<Agenda />} />
-            <Route path="/diario" element={<Diario />} />
-            <Route path="/conteudos" element={<Conteudos />} />
-            <Route path="/prontuario" element={<Prontuario />} />
-            <Route path="/admin" element={<AdminPanel />} />
+            <Route path="/check-in-periodico" element={<CheckinPeriodico />} />
+            <Route path="/completar-perfil" element={<CompletarPerfil />} />
+            <Route 
+              path="/jornada" 
+              element={
+                <ProtectedRoute requireAssignedLine={true}>
+                  <Jornada />
+                </ProtectedRoute>
+              } 
+            />
+            <Route 
+              path="/agenda" 
+              element={
+                <ProtectedRoute requireAssignedLine={true}>
+                  <Agenda />
+                </ProtectedRoute>
+              } 
+            />
+            <Route 
+              path="/diario" 
+              element={
+                <ProtectedRoute requireAssignedLine={true}>
+                  <Diario />
+                </ProtectedRoute>
+              } 
+            />
+            <Route 
+              path="/conteudos" 
+              element={
+                <ProtectedRoute requireAssignedLine={true}>
+                  <Conteudos />
+                </ProtectedRoute>
+              } 
+            />
+            <Route 
+              path="/prontuario" 
+              element={
+                <ProtectedRoute requirePatientProfile={true}>
+                  <Prontuario />
+                </ProtectedRoute>
+              } 
+            />
+            <Route 
+              path="/perfil" 
+              element={
+                <ProtectedRoute requirePatientProfile={true}>
+                  <Perfil />
+                </ProtectedRoute>
+              } 
+            />
+            <Route 
+              path="/notificacoes" 
+              element={
+                <ProtectedRoute>
+                  <Notificacoes />
+                </ProtectedRoute>
+              } 
+            />
+            <Route 
+              path="/admin" 
+              element={
+                <ProtectedRoute requireAdmin={true}>
+                  <AdminPanel />
+                </ProtectedRoute>
+              } 
+            />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
