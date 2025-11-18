e454890f0130c794e3b170508d3d6f0fac3a30a7
Eduardo Novais <eduardodepaulanovais10@gmail.com>
2025-11-18 10:01:50 -0300

    chore: atualiza configurações base

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
-import { FAQButton } from "@/components/FAQButton";
+import { GlobalNotificationWrapper } from "@/components/layout/GlobalNotificationWrapper";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import Cadastro from "./pages/Cadastro";
@@ -116,7 +116,7 @@ const App = () => (
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-          <FAQButton />
+          <GlobalNotificationWrapper />
         </BrowserRouter>
       </TooltipProvider>
     </AuthProvider>
